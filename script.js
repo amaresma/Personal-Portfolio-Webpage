c8fcfa7b0eb22761cd4352c31e6e66f3b58c4aca
Albert Maresma <albertmaresma@Zimones-MacBook-Air.local>
2020-09-05 13:25:52 +0200

    added functionallity to the webpage

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/* Toggle */
+
 window.addEventListener("load", () => {
   document.body.classList.remove("preload");
 });
@@ -13,3 +16,34 @@ document.addEventListener("DOMContentLoaded", () => {
     nav.classList.remove("nav--open");
   });
 });
+
+/* Content show & hide */
+
+var li_elements = document.querySelectorAll(".nav .nav__links .nav__link");
+var item_elements = document.querySelectorAll(".item");
+for (var i = 0; i < li_elements.length; i++) {
+  li_elements[i].addEventListener("click", function() {
+    li_elements.forEach(function(li) {
+      li.classList.remove("active");
+    });
+    this.classList.add("active");
+    var li_value = this.getAttribute("data-li");
+    item_elements.forEach(function(item) {
+      item.style.display = "none";
+    });
+    if (li_value == "about") {
+      document.querySelector("." + li_value).style.display = "block";
+    } else if (li_value == "portfolio") {
+      document.querySelector("." + li_value).style.display = "block";
+    } else if (li_value == "social-media") {
+      document.querySelector("." + li_value).style.display = "block";
+    } else if (li_value == "blog") {
+      document.querySelector("." + li_value).style.display = "block";
+    } else if (li_value == "contact") {
+      document.querySelector("." + li_value).style.display = "block";
+    } else {
+      console.log("");
+    }
+  });
+}
+
